6918f156a4687907d005f3d4f87e1462f21ec398
saibakho <mingyehchou@yahoo.com.tw>
2023-03-12 06:04:12 +0800

    [post] contig merging done

--- postprocess.cpp
+++ postprocess.cpp
@@ -1,11 +1,14 @@
 #include "utils.cpp"
+#include <utility>
+using std::pair;
 
 vector<Marker> tar;
-vector<vector<string>> scaffolds;
+vector<vector<pair<string, int>>> scaffolds;
 
 map<int, int> joins;
-map<string, int> visited;
+map<string, bool> visited;
 map<string, Telos> contig2telos;
+map<string, vector<pair<string, int>>> mergeContig;
 
 int main(int argc, char *argv[])
 {
@@ -38,26 +41,23 @@ int main(int argc, char *argv[])
 	// scaffolding
 	for (auto& p: visited) {
 		Telos tp = contig2telos[p.first];
-		if (joins.count(tp.lhs) == 0 && joins.count(tp.rhs) == 0 ||
-			p.second != 0)
+		vector<pair<string, int>> curScaffold;
+		if (joins.count(tp.lhs) == 0 && joins.count(tp.rhs) == 0 || p.second)
 			continue;
-		vector<string> curScaffold;
-
 		// traverse scaffolds
 		string curContig = p.first;
 		int curTelo = joins.count(tp.lhs) ? tp.rhs : tp.lhs;
-		while (p.second == 0) {
+		while (!p.second) {
 			int nextTelo = joins[curTelo];
 			string nextContig = tar[abs(nextTelo)-1].contig;
 
+			Telos ctp = contig2telos[curContig];
 			Telos ntp = contig2telos[nextContig];
-			if (nextTelo == ntp.lhs) {
-				visited[nextContig] = 1;
-				curTelo = ntp.rhs;
-			} else {
-				visited[nextContig] = -1;
-				curTelo = ntp.lhs;
-			}	curScaffold.push_back(curContig);
+			curScaffold.push_back(make_pair(curContig, curTelo == ctp.lhs ? 1 : 0));
+			//curScaffold.push_back(make_pair(nextContig, nextTelo == ntp.lhs ? 0 : 1));
+			curTelo = nextTelo == ntp.lhs ? ntp.rhs : ntp.lhs;
+
+			visited[nextContig] = true;
 			curContig = nextContig;
 		}	scaffolds.push_back(curScaffold);
 	}
@@ -65,14 +65,27 @@ int main(int argc, char *argv[])
 	// add reduced contigs
 	fin.open(out_dir+"/removed_spd1.txt");
     while (fin >> contig) {
-		scaffolds.push_back(vector<string>(1, contig));
+		scaffolds.push_back(vector<pair<string, int>>(1, make_pair(contig, 0)));
 	}	fin.close();
 
 	// merge contigs
 	fin.open(out_dir+"/tar_merge.txt");
-    while (fin >> contig) {
-	
+	string key;
+    while (fin >> key) {
+		vector<pair<string, int>> seg;
+		while (fin >> contig) {
+			if (contig == "end")
+				break;
+			fin >> tmp;
+			seg.push_back(make_pair(contig, tmp));
+		}	mergeContig[key] = seg;
 	}	fin.close();
+
+	for (auto& p: mergeContig) {
+		print("{}:\n", p.first);
+		for (auto& cp: p.second)
+			print("  {} {}\n", cp.first, cp.second);
+	}
 
 	// cut cycles
 
@@ -80,9 +93,24 @@ int main(int argc, char *argv[])
 	ofstream fout(out_dir+"/scaffolds.txt");
 	for (int i = 0; i < scaffolds.size(); i++) {
 		fout << format("> Scaffold_{}\n", i+1);
-		for (string& s: scaffolds[i])
-			fout << format("{} {}\n", s, visited[s] > -1 ? 0 : 1);
-		fout << "\n";
+		for (auto& sp: scaffolds[i]) {
+			auto it = mergeContig.find(sp.first);
+			if (it != mergeContig.end()) {
+				int sign;
+				for (auto& mp: it->second)
+					if (mp.first == sp.first)
+						sign = mp.second;
+				print("{}: {}, {}\n", sp.first, sp.second, sign);
+				if (sp.second + sign == 1) {
+					for (int i = it->second.size()-1; i > -1; i--)
+						fout << format("{} {}\n", it->second[i].first, 1-it->second[i].second);
+				} else {
+					for (auto& mp: it->second)
+						fout << format("{} {}\n", mp.first, mp.second);
+				}
+			} else
+				fout << format("{} {}\n", sp.first, sp.second);
+		}	fout << "\n";
 	}	fout.close();
 	return 0;
 }
